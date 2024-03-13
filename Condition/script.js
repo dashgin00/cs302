b0b3ea0f48ef3ac56a3f80f269cbfa6e81c24716
Rahimov Dashgin <dashginkr@code.local>
2024-03-13 19:25:27 +0400

    first commit

---
+++ Condition/script.js
@@ -0,0 +1,23 @@
+let userName = 'dashgin'
+let lastName = 'rahimov'
+let age = 16 
+let isAdmin = true
+let year = 2008
+
+if (age >= 20 )
+console.log(1);
+else if (userName == 'ulfet' || isAdmin  == true || userName == 'Azerin')
+console.log(2);
+else
+console.log(3);
+
+if (userName == lastName || year != 16) {
+    console.log('THIR IS AN ERROR YOU ARE ROBOT'); }
+else
+    console.log('QƏBUL EDİLDİNİZ');
+
+
+    let uName = 'dashgin rahimov'
+    console.log(uName.charAt(0).toUpperCase(0) + uName.slice(1) + uName.split(-7).toUpperCase(-7));
+
+
